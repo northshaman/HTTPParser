cb5d7b1512a9fcbdde22e1ee1b52c76d1ab9722e
northshaman <north-shaman@mail.ru>
2018-07-05 15:50:18 +0500

    fill configs add IMPORTANT Dependency for phantom web driver

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -1,7 +1,16 @@
 package config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+
 /**
- * Created by shaman on 05.07.2018.
+ * Main Java Configuration class
  */
+@Configuration
+@PropertySource("classpath:config.properties")
+@ComponentScan({"services","utils"})
+@Import(DBConfig.class)
 public class AppConfig {
 }
--- src/main/java/config/DBConfig.java
+++ src/main/java/config/DBConfig.java
@@ -1,7 +1,12 @@
 package config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
 /**
- * Created by shaman on 05.07.2018.
+ * Java configuration class for DB
  */
+@Configuration
+@PropertySource("classpath:dbprops.properties")
 public class DBConfig {
 }
