c6873fb9d84ecd328395376dbf3b5353f0b2d521
northshaman <north-shaman@mail.ru>
2018-07-05 15:42:16 +0500

    clean some devDep add Config Classes

--- src/main/java/config/AppInit.java
+++ src/main/java/config/AppInit.java
@@ -1,7 +1,28 @@
 package config;
 
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
 /**
- * Created by shaman on 05.07.2018.
+ * Initializer
  */
-public class AppInit   {
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{
+                AppConfig.class
+        };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{
+                WebConfig.class
+        };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
